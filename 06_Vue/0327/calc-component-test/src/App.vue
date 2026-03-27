86dbf251d983fa85fca9bf2488b5eafd7fd8f80e
hyeon98 <swhyeon98@gmail.com>
2026-03-27 13:06:05 +0900

    오전 수업 실습 (누락 커밋)

---
+++ 06_Vue/0327/calc-component-test/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <Calc />
+  </div>
+</template>
+<script>
+import Calc from './components/Calc6.vue';
+export default {
+  name: 'App',
+  components: { Calc },
+};
+</script>
