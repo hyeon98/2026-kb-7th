d4d5150bd791ab1f00c8b252aee20f8cbd915152
hyeon98 <swhyeon98@gmail.com>
2026-04-12 14:27:27 +0900

    누락 커밋

---
+++ 06_Vue/0402/axios-test-app/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>콘솔 확인</h2>
+  </div>
+</template>
+
+<script setup>
+import axios from 'axios';
+
+const requestApi = () => {
+  const url = 'http://localhost:3000/todos/1';
+  axios.get(url).then((response) => {
+    console.log('# 응답객체: ', response);
+    console.log(typeof response.data);
+  });
+};
+
+requestApi();
+</script>
---
+++ 06_Vue/0402/rest-api/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <button type="button" @click="postData">POST</button><br />
+    <button type="button" @click="getData">GET</button> <br />
+    <button type="button">PUT</button> <br />
+    <button type="button">PATCH</button> <br />
+  </div>
+</template>
+
+<script setup>
+import axios from 'axios';
+const getData = () => {
+  axios
+    .get('/api')
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch(() => {
+      console.log(`데이터 가져오다가 에러:`, error);
+    });
+};
+
+const postData = () => {
+  axios
+    .post('/api/data', { name: `new Data`, description: `sample` })
+    .then(() => {})
+    .catch((error) => {
+      console.log(`ㅎㅇ ERROR: `, error);
+    });
+};
+
+const putData = () => {
+  axios
+    .put('/api/data', { name: `new Data_PUT`, description: `sample` })
+    .then(() => {})
+    .catch(() => {
+      console.log(`ㅎㅇ ERROR: `, error);
+    });
+};
+
+const patchData = () => {
+  axios
+    .patch('/api/data', { name: `new Data_PATCH`, description: `sample` })
+    .then(() => {})
+    .catch((error) => {
+      console.log(`ㅎㅇ ERROR: `, error);
+    });
+};
+</script>
