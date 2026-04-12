6e12a9be83ac028b52b9239e404ef834e5b7ff80
hyeon98 <swhyeon98@gmail.com>
2026-04-12 16:40:46 +0900

    fix: 잘못된 경로 설정으로 인한 404 오류 해결

--- 06_Vue/0412/vue-board-pbl-student-todo/vite.config.js
+++ 06_Vue/0412/vue-board-pbl-student-todo/vite.config.js
@@ -16,7 +16,7 @@ export default defineConfig({
       // 1) '/api'로 시작하는 요청을 json-server(3000번 포트)로 전달하세요.
       // 2) '/api/boards' -> 실제 서버에서는 '/boards'가 되도록 rewrite 하세요.
 
-      api: {
+      '/api': {
         target: 'http://localhost:3000',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
